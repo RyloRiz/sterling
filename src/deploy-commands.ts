9fb79b035817261c8c19650548192806d6997c94
RyloRiz <91710711+RyloRiz@users.noreply.github.com>
2022-11-22 02:52:50 -0800

    setup & ping cmd

---
+++ src/deploy-commands.ts
@@ -0,0 +1,31 @@
+import fs from 'node:fs'
+import { REST, Routes } from 'discord.js'
+
+const { CLIENTID, TOKEN } = process.env;
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(TOKEN as string);
+
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			// Routes.applicationGuildCommands(CLIENTID as string, GUILDID as string),
+			Routes.applicationCommands(CLIENTID as string),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${(data as any).length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
