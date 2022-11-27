5654cdb9c738174bf60648bc9f495e58e3faf519
RyloRiz <91710711+RyloRiz@users.noreply.github.com>
2022-11-26 17:02:25 -0800

    testing ci/cd #1 & updated ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,9 +3,16 @@ import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong!')
+		.addBooleanOption(option =>
+			option
+				.setName('ephemeral')
+				.setDescription('Whether or not to make this view-only by yourself')),
 
 	async execute(interaction: CommandInteraction) {
-		await interaction.reply('Pong!');
+		await interaction.reply({
+			content: 'Pong!',
+			ephemeral: interaction.options.get('ephemeral')?.value as boolean || false
+		});
 	},
 };
